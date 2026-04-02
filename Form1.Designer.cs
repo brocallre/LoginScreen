7decc9bdca9eff57f7e2822b60d73f822d245584
brocallre <hyt83532342@gmail.com>
2026-04-02 10:09:24 +0900

    과제1 코딩과 테스트 완료 ListBox를 중앙에 TextBox를 ListBox 아래에 배치함 전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LoginScreen
+namespace LoginScreen
 {
     partial class Form1
     {
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitle.Location = new System.Drawing.Point(135, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(116, 45);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "로그인";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtId.Location = new System.Drawing.Point(50, 100);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(300, 29);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPassword.Location = new System.Drawing.Point(50, 140);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(300, 29);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLogin.Location = new System.Drawing.Point(50, 200);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(300, 45);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "로그인";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "로그인 화면";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LoginScreen
 {
     public partial class Form1 : Form
     {
+        // 텍스트박스 기본 안내문구(Placeholder) 설정
+        private string idPlaceholder = "아이디를 입력하세요";
+        private string pwPlaceholder = "비밀번호를 입력하세요";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 프로그램이 켜졌을 때 처음으로 나올 화면 설정
+            SetPlaceholder(txtId, idPlaceholder);
+            SetPlaceholder(txtPassword, pwPlaceholder);
+
+            // 텍스트박스에 마우스 클릭(포커스) 이벤트 연결
+            txtId.GotFocus += TxtId_GotFocus;
+            txtId.LostFocus += TxtId_LostFocus;
+
+            txtPassword.GotFocus += TxtPassword_GotFocus;
+            txtPassword.LostFocus += TxtPassword_LostFocus;
+
+            // 시작하자마자 텍스트박스에 커서가 가는 것을 방지하기 위해 로고(레이블)에 포커스를 줌
+            this.ActiveControl = lblTitle;
+        }
+
+        // 안내문구를 보여주는 함수
+        private void SetPlaceholder(TextBox txt, string placeholder)
+        {
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                txt.Text = placeholder;
+                txt.ForeColor = Color.Gray;
+
+                // 비밀번호 칸일 경우 힌트 글씨가 별표로 바뀌지 않게 해제
+                if (txt == txtPassword)
+                {
+                    txt.PasswordChar = '\0';
+                }
+            }
+        }
+
+        // 안내문구를 지우는 함수
+        private void RemovePlaceholder(TextBox txt, string placeholder)
+        {
+            if (txt.Text == placeholder)
+            {
+                txt.Text = "";
+                txt.ForeColor = Color.Black;
+
+                // 비밀번호 칸에 글자를 입력할 때부터 별표 표시
+                if (txt == txtPassword)
+                {
+                    txt.PasswordChar = '*';
+                }
+            }
+        }
+
+        private void TxtId_GotFocus(object sender, EventArgs e)
+        {
+            RemovePlaceholder(txtId, idPlaceholder);
+        }
+
+        private void TxtId_LostFocus(object sender, EventArgs e)
+        {
+            SetPlaceholder(txtId, idPlaceholder);
+        }
+
+        private void TxtPassword_GotFocus(object sender, EventArgs e)
+        {
+            RemovePlaceholder(txtPassword, pwPlaceholder);
+        }
+
+        private void TxtPassword_LostFocus(object sender, EventArgs e)
+        {
+            SetPlaceholder(txtPassword, pwPlaceholder);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string id = txtId.Text;
+            string pw = txtPassword.Text;
+
+            // 아직 아무것도 입력하지 않은 상태(힌트 문구)라면 빈 값으로 인식
+            if (id == idPlaceholder) id = "";
+            if (pw == pwPlaceholder) pw = "";
+
+            // 아이디와 비밀번호가 모두 맞는지 확인 (&& 연산자 사용)
+            if (id == "admin" && pw == "1234")
+            {
+                MessageBox.Show("로그인 성공!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 비밀번호가 틀렸습니다.", "실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
